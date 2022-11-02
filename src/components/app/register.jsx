4c9bfd2d289be45d1c3c8dcae0be2f8762e0bcb1
Omadbek2009 <toxtasinovomadbek054@gmail.com>
2022-11-02 15:55:28 +0500

    Beckend

--- src/components/app/register.jsx
+++ src/components/app/register.jsx
@@ -3,20 +3,21 @@ import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
+import { Fade } from 'react-reveal';
 function Register() {
     return (
         <>
             <div className="home">
                 <div className="login">
                     <div className="inp">
-                        <div className="h1"><h1>Regis<b>ter.</b></h1></div>
-                        <input type="text" placeholder='LastName' />
-                        <input type="text" placeholder='FirstName' />
-                        <input type="email" placeholder='Email' />
-                        <input type="password" placeholder='Password' />
+                        <div className="h1"><Fade top><h1>Regis<b>ter.</b></h1></Fade></div>
+                        <Fade left><input type="text" placeholder='LastName' /></Fade>
+                        <Fade right><input type="text" placeholder='FirstName' /></Fade>
+                        <Fade left><input type="email" placeholder='Email' /></Fade>
+                        <Fade right><input type="password" placeholder='Password' /></Fade>
                         <Stack spacing={2} direction="row">
-                            <Link to="login"><Button variant="outlined">Login In</Button></Link>
-                            <Link><Button variant="outlined">Submit</Button></Link>
+                            <Fade left><Link to="login"><Button variant="outlined">Login In</Button></Link></Fade>
+                            <Fade right><Link><Button variant="outlined">Submit</Button></Link></Fade>
                         </Stack>
                     </div>
                 </div>
